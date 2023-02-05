28a0113861a7c16686096caf75ad8c5e3117fbb7
AvishParmar <avishparmar53@gmail.com>
2023-02-05 18:44:24 -0500

    Subregion editing implemented

--- src/App.js
+++ src/App.js
@@ -1,55 +1,73 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap, GeoJSON } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  GeoJSON,
+} from "react-leaflet";
 import { useState } from "react";
 import * as L from "leaflet";
 import "./App.css";
 import teslaData from "./data/tesla-sites.json";
-import geoData from "./data/geo.json"
+import geoData from "./data/geo.json";
 
 function App() {
-
   const [files, setFiles] = useState("");
-  const filteredStations = teslaData.filter(
-    (tesla) => tesla.address.country === "Italy"
-  );
-  
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     const fileReader = new FileReader();
-  //   fileReader.onloadend = ()=>{
-  //     setFiles(JSON.parse(fileReader.result));
-  //  }
+    //   fileReader.onloadend = ()=>{
+    //     setFiles(JSON.parse(fileReader.result));
+    //  }
     fileReader.readAsText(e.target.files[0], "UTF-8");
-    fileReader.onload = e => {
+    fileReader.onload = (e) => {
       //console.log("e.target.result", e.target.result);
       setFiles(JSON.parse(e.target.result));
     };
   };
 
-  const clickHandler = e => {
+  const clickHandler = (e) => {
+    let h2 = document.getElementById(e.target.id);
+    let h2Text = h2.innerText.substring(11);
+    h2.innerHTML = "<input type='text' id='input' value='"+h2Text+"'/>";
+    let input = document.getElementById("input");
+    input.focus();
 
-  }
+    input.addEventListener("blur", function () {
+      let newText = "Subregion: " + input.value;
+      h2.textContent = newText;
+      h2.innerHTML = newText;
+    });
+    input.addEventListener("keypress", function(e) {
+      if(e.key === 'Enter'){
+        let newText = "Subregion: " + input.value;
+        h2.textContent = newText;
+        h2.innerHTML = newText;
+      }
+    })
+  };
 
-
-  if(files === ""){
+  if (files === "") {
     return (
       <div>
         <h1>Welcome to GeoJSON Viewer, please upload your file</h1>
         <input type="file" onChange={handleChange} />
       </div>
     );
-  }
-  else {
-    console.log(files.features[0].properties.subregion)
+  } else {
+    console.log(files.features[0].properties.subregion);
     var features = files.features;
+
     return (
-    
       <MapContainer
-        center={[features[0].properties.label_y, features[0].properties.label_x]}
+        center={[
+          features[0].properties.label_y,
+          features[0].properties.label_x,
+        ]}
         zoom={6}
         scrollWheelZoom={true}
       >
-        
-        
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -57,14 +75,23 @@ function App() {
         {/* <GeoJSON data = {geoData}/> */}
         {features.map((feature) => (
           <Marker
-            
             position={[feature.properties.label_y, feature.properties.label_x]}
           >
-            <Popup position={[feature.properties.label_y, feature.properties.label_x]}>
+            <Popup
+              position={[
+                feature.properties.label_y,
+                feature.properties.label_x,
+              ]}
+            >
               <div>
-                <h2 onDoubleClick={clickHandler()}>{"Subregion: " + feature.properties.subregion}</h2>
+                <h2
+                  id={feature.properties.pop_rank}
+                  onDoubleClick={clickHandler}
+                >
+                  {"Subregion: " + feature.properties.subregion}
+                </h2>
                 <p>{"Continent :" + feature.properties.continent}</p>
-                <p>{"Name: "+ feature.properties.name}</p>
+                <p>{"Name: " + feature.properties.name}</p>
               </div>
             </Popup>
           </Marker>
